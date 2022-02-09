f6c006903a3b254010aa6140dc4e39c950609574
JoseLValdes <jolvaldgo@gmail.com>
2022-02-09 19:37:32 +0100

    Components: Header & Gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LayoutModule } from './components/layout/layout.module';
+import { HeaderComponent } from './components/header/header.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    GalleryComponent,
   ],
   imports: [
     BrowserModule,
