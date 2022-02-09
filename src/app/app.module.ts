061592080d12ea7b21aca6e68d4a0d8bfe17bfac
JoseLValdes <jolvaldgo@gmail.com>
2022-02-09 19:51:16 +0100

    About Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { LayoutModule } from './components/layout/layout.module';
 import { HeaderComponent } from './components/header/header.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     GalleryComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
