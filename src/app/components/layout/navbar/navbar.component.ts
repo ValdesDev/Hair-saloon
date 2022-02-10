502e41239dfc083ee084c5037d632f90303c98c5
JoseLValdes <jolvaldgo@gmail.com>
2022-02-10 17:05:05 +0100

    Images Navbar refactor

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -3,9 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
-
+  navbarList = [
+    { path: '#section-home', name: 'Home' },
+    { path: '#section-portfolio', name: 'Peinados' },
+    { path: '#section-about', name: 'Sobre Nosotras' },
+    { path: '#section-contact', name: 'Opiniones' },
+    { path: '#section-services', name: 'Servicios' },
+  ];
 }
